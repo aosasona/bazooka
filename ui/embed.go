80ea7a4e9e51e35b2e5c34c0f414dc69bd889261
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-01-28 17:35:07 +0000

    Cleaned up embed.go

--- ui/embed.go
+++ ui/embed.go
@@ -2,21 +2,9 @@ package ui
 
 import (
 	"embed"
-	"io/fs"
 )
 
 //go:generate yarn
 //go:generate yarn build
 //go:embed dist/*
 var UIDir embed.FS
-var (
-	UIFS fs.FS
-)
-
-func init() {
-	var err error
-	UIFS, err = fs.Sub(UIDir, "dist")
-	if err != nil {
-		panic(err)
-	}
-}
